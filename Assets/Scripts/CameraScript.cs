d90ffb97e67e7cd84f89a7ac52607d8c3e59f3e9
James Li <sljamesli@yahoo.de>
2019-08-08 11:34:17 +0900

    Upload local files

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Transform playerTrans;
+    private Vector3 offset;
+
+
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    private float smoothness = 0.1f;
+    [SerializeField]
+    private float rotationspeed = 0f;
+    
+
+    void Start()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - playerTrans.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        Quaternion rot = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationspeed, Vector3.up);
+        offset = rot * offset;
+
+        Vector3 newPos = playerTrans.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothness);
+        transform.LookAt(playerTrans);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    private Transform mytrans;
+    private Rigidbody rb;
+    private Collider mycollider;
+    private Animator anim;
+    private float dtog = 0f;
+    private int djump = 0;
+
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float backpaddle = 0.5f;
+    [SerializeField] private float jumpForce = 0f;
+    [SerializeField] private float fallmult = 1.5f;
+    [SerializeField] private float lowmult = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mytrans = GetComponent<Transform>();
+        mycollider = GetComponent<Collider>();
+        anim = GetComponent<Animator>();
+        dtog = mycollider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((isGrounded()|| djump < 1) && Input.GetKeyDown("space"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            djump = isGrounded() ? 0 : djump + 1;
+        }
+
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallmult * Time.deltaTime;
+
+        }
+        else if(rb.velocity.y > 0 && !Input.GetKey("space"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * lowmult * Time.deltaTime;
+        }
+
+        anim.SetFloat("inputH", Input.GetAxis("Horizontal"));
+        anim.SetFloat("inputV", Input.GetAxis("Vertical"));
+    }
+
+    bool isGrounded()
+    {
+        return Physics.Raycast(mytrans.position, -Vector3.up, dtog + 0.1f);
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            //rb.AddForce(0f, 0f, Input.GetAxis("Vertical") * moveForce);              //starts too slow
+            rb.velocity = Vector3.forward * Input.GetAxis("Vertical") * moveSpeed;
+            Debug.Log(rb.velocity);
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            rb.velocity = Vector3.forward * Input.GetAxis("Vertical") * moveSpeed * backpaddle;
+        }
+
+            if (Input.GetButton("Horizontal"))
+        {
+            //rb.AddForce(Input.GetAxis("Horizontal") * moveSpeed, 0f, 0f);
+            rb.velocity = Vector3.right * Input.GetAxis("Horizontal") * moveSpeed;
+        }
+
+    }
+}
